c4b52ae2ac9270424236c432863ca3da3c659619
Ariana Fernandez <arianafm@ciencias.unam.mx>
2021-03-18 17:46:07 -0600

    Subiendo filtros tarea2

--- index.js
+++ index.js
@@ -401,6 +401,24 @@ function filtrosT2(elemento) {
     case "componentesRGB":
         context.putImageData(componentesRGB(imageData),0,0);
         break;
+    case "softBlur":
+        softBlur();
+        break;
+    case "blur":
+        blur();
+        break;
+    case "motionBlur":
+        motionBlur();
+        break;
+    case "findEdges":
+        findEdges();
+        break;
+    case "sharpen":
+        sharpen();
+        break;
+    case "emboss":
+        emboss();
+        break;
   }
 }
 
@@ -489,4 +507,157 @@ function componentesRGB(imageData) {
   
   return imageData;
 
+}
+
+
+function convolucion(matriz){
+  var context = newCanvas.getContext("2d");
+  var imageData = context.getImageData(0, 0, newCanvas.width, newCanvas.height);
+  let newImageData = context.createImageData(newCanvas.width, newCanvas.height);
+  var data = imageData.data;
+
+  let margen_x = Math.floor(matriz.tamano_n / 2);
+  let margen_y = Math.floor(matriz.tamano_m / 2);
+
+  let entrada_x, entrada_y;
+
+  let color_index;
+  let new_color;
+
+  function auxColorIndex(x, y, width) {
+    let color_pos = x * 4 + y * (width * 4);
+    return { R: color_pos, G: color_pos + 1, B: color_pos + 2, A: color_pos + 3 };
+  }
+
+  for (let i = margen_x; i < newCanvas.width - margen_x; i++) {
+    for (let j = margen_y; j < newCanvas.height - margen_y; j++) {
+        new_color = { R: 0, G: 0, B: 0 };
+
+        for (let entrada = 0; entrada < matriz.valores.length; entrada++) {
+            entrada_x = (entrada % matriz.tamano_n) - margen_x;
+            entrada_y = Math.floor(entrada / matriz.tamano_n) - margen_y;
+
+            color_index = auxColorIndex(i + entrada_x, j + entrada_y, newCanvas.width);
+
+            new_color.R += data[color_index.R] * matriz.valores[entrada];
+            new_color.G += data[color_index.G] * matriz.valores[entrada];
+            new_color.B += data[color_index.B] * matriz.valores[entrada];
+        }
+
+        color_index = auxColorIndex(i, j, newCanvas.width);
+        newImageData.data[color_index.R] = new_color.R;
+        newImageData.data[color_index.G] = new_color.G;
+        newImageData.data[color_index.B] = new_color.B;
+        newImageData.data[color_index.A] = data[color_index.A];
+    }
+  }
+
+  context.putImageData(newImageData,0,0)
+
+}
+
+// Soft Blur: Desenfoca suavemente la imagen. Completamos 5 veces el valor 0.2 en el filtro.
+function softBlur() {
+  let matriz = {
+      valores:
+          [
+              0.0, 0.2, 0.0,
+              0.2, 0.2, 0.2,
+              0.0, 0.2, 0.0
+          ],
+          tamano_n: 3, tamano_m: 3
+  }
+
+  convolucion(matriz);
+}
+
+// Blur: Desenfoca la imagen. El factor (13) se divide por la suma de todos los elementos.
+function blur() {
+  let matriz = {
+      valores:
+          [
+                0,    0,  1 /13,    0,    0,
+                0,  1/13,  1/13, 1/13,    0,
+              1/13, 1/13,  1/13, 1/13, 1/13,
+                0,  1/13,  1/13, 1/13,    0,
+                0,      0, 1/13,    0,    0
+          ],
+          tamano_n: 5, tamano_m: 5
+  }
+
+  convolucion(matriz);
+}
+
+// Motion Blur: Difuminamos en una sola dirección. El siguiente es un
+// filtro de desenfoque de movimiento de 9x9.
+function motionBlur() {
+  let matriz = {
+      valores:
+          [
+              1/9,   0,   0,   0,   0,   0,   0,   0,  0,
+                0, 1/9,   0,   0,   0,   0,   0,   0,  0,
+                0,   0, 1/9,   0,   0,   0,   0,   0,  0,
+                0,   0,   0, 1/9,   0,   0,   0,   0,  0,
+                0,   0,   0,   0, 1/9,   0,   0,   0,  0,
+                0,   0,   0,   0,   0, 1/9,   0,   0,  0,
+                0,   0,   0,   0,   0,   0, 1/9,   0,  0,
+                0,   0,   0,   0,   0,   0,   0, 1/9,  0,
+                0,   0,   0,   0,   0,   0,   0,   0, 1/9
+          ],
+          tamano_n: 9, tamano_m: 9
+  }
+
+  convolucion(matriz);
+}
+
+// Find Edges: Filtro para encontrar bordes de 45°.
+function findEdges() {
+  let matriz = {
+      valores:
+          [
+            -1,  0,  0,  0,  0,
+            0, -2,  0,  0,  0,
+            0,  0,  6,  0,  0,
+            0,  0,  0, -2,  0,
+            0,  0,  0,  0, -1,
+          ],
+          tamano_n: 5, tamano_m: 5
+  }
+
+  convolucion(matriz);
+}
+
+// Sharpen: Imagen con el mismo brillo que el original pero más nítida. 
+function sharpen() {
+  let matriz = {
+      valores:
+          [
+              -1, -1, -1,
+              -1,  9, -1,
+              -1, -1, -1
+          ],
+          tamano_n: 3, tamano_m: 3
+  }
+
+  convolucion(matriz);
+}
+
+
+// Emboss: Efecto de sombra 3D a la imagen, el resultado es muy útil para un mapa 
+// de relieve de la imagen. En este caso los valores nos proporcionan un filtro de 
+// relieve muy exagerado.
+function emboss() {
+  let matriz = {
+      valores:
+          [
+              -1, -1, -1, -1, 0,
+              -1, -1, -1,  0, 1,
+              -1, -1,  0,  1, 1,
+              -1,  0,  1,  1, 1,
+               0,  1,  1,  1, 1
+          ],
+          tamano_n: 5, tamano_m: 5
+  }
+
+  convolucion(matriz);
 }
